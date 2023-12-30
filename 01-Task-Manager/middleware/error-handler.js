87bc1dab3a24f961b915c294cadf2ad599d697b8
PythonXHunter <110535689+PythonXHunter@users.noreply.github.com>
2023-12-30 18:06:55 +0530

    Add files via upload

---
+++ 01-Task-Manager/middleware/error-handler.js
@@ -0,0 +1,10 @@
+const { CustomAPIError } = require('../error/custom-error')
+
+const errorHandlerMiddleware = (err, req, res, next) => {
+	if( err instanceof CustomAPIError){
+		return res.status(err.status).json({ msg : err.message})
+	}
+	return res.status(500).json({ message: 'Something went wrong, please try again later' })
+}
+
+module.exports = errorHandlerMiddleware
